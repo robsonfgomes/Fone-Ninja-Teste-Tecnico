5f16883e44e14d35aa88d4116264808f0835f790
Robson Gomes <fgomes.robson@gmail.com>
2026-06-24 00:11:34 -0300

    fix: Criação de parâmetros para permitir informar a página e quantidade de produtos por página

--- backend/app/Actions/Product/ListProductsAction.php
+++ backend/app/Actions/Product/ListProductsAction.php
@@ -2,13 +2,17 @@
 
 namespace App\Actions\Product;
 
+use App\Dtos\Product\ListProductsDto;
 use App\Models\Product\Product;
 use Illuminate\Pagination\LengthAwarePaginator;
 
 class ListProductsAction
 {
-    public function execute(): LengthAwarePaginator
+    public function execute(ListProductsDto $dto): LengthAwarePaginator
     {
-        return Product::query()->orderBy('name')->paginate();
+        return Product::query()->orderBy('name')->paginate(
+            perPage: $dto->perPage,
+            page: $dto->page,
+        );
     }
 }
